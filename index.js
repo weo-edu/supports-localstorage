82f1f99d74cabc05a00b82d8c8ca4a848e69a016
Andrew Shaffer <andrew@weo.io>
2015-02-27 12:56:29 -0800

    add memoization

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 var storageName = 'localStorage';
 var testKey = 'supports-localStorage_test';
 
-module.exports = function() {
-  var storage = window[storageName];
+var memo;
 
-  if(storage) {
-    try {
-      storage.setItem(testKey, true);
-      storage.removeItem(testKey);
-      return true;
-    } catch(e) {
-      return false;
-    }
+module.exports = function() {
+  // We can memoize this since supporting localstorage or not
+  // is invariant once we are in a particular environment
+  if(memo === undefined) {
+    var storage = window[storageName];
+
+    if(storage) {
+      try {
+        storage.setItem(testKey, true);
+        storage.removeItem(testKey);
+        memo = true;
+      } catch(e) {
+        memo = false;
+      }
+    } else
+      memo = false;
   }
 
   return false;
