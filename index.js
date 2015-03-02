ff8f905a9f32268def140a0c7be13084b3aa05f4
Andrew Shaffer <darawk@gmail.com>
2015-03-02 01:42:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var memo;
 module.exports = function() {
   // We can memoize this since supporting localstorage or not
   // is invariant once we are in a particular environment
-  if('undefined' !== typeof window || memo === undefined) {
+  if('undefined' !== typeof window && memo === undefined) {
     var storage = window[storageName];
     if(storage) {
       try {
