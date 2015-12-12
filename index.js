5079467ef42c298cf44ed0dc4c14b28a7243b66d
Andrew Shaffer <andrew@weo.io>
2015-12-11 19:59:38 -0800

    dont even access the localStorage property outside a try-catch

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-var storageName = 'localStorage';
-var testKey = 'supports-localStorage_test';
+var storageName = 'localStorage'
+var testKey = 'supports-localStorage_test'
 
-var memo;
+var memo
 
 module.exports = function() {
   // We can memoize this since supporting localstorage or not
   // is invariant once we are in a particular environment
   if(memo === undefined) {
-    var storage = window[storageName];
-    if(storage) {
-      try {
-        storage.setItem(testKey, true);
-        storage.removeItem(testKey);
-        memo = true;
-      } catch(e) {
-        memo = false;
+    try {
+      var storage = window[storageName]
+      if(storage) {
+        storage.setItem(testKey, true)
+        storage.removeItem(testKey)
+        memo = true
+      } else {
+        memo = false
       }
-    } else
-      memo = false;
+    } catch (e) {
+      memo = false
+    }
   }
 
-  return memo;
-};
+  return memo
+}
--- test/index.js
+++ test/index.js
@@ -1,40 +1,40 @@
-var expect = require('chai').expect;
+var expect = require('chai').expect
 
 // Simulate browser
-global.window = {};
+global.window = {}
 
 describe('supports-localStorage', function() {
   var supportsLocalstorage =
 
   beforeEach(function() {
-    supportsLocalstorage = require('../');
-  });
+    supportsLocalstorage = require('../')
+  })
 
   // supports-localStorage memoizes itself, so need to clear
   // the require cache each time to test properly
   afterEach(function() {
     Object.keys(require.cache).forEach(function(key) {
-      delete require.cache[key];
-    });
-  });
+      delete require.cache[key]
+    })
+  })
 
   it('should report false localStorage is undefined', function() {
-    expect(supportsLocalstorage()).to.be.false;
-  });
+    expect(supportsLocalstorage()).to.be.false
+  })
 
   var mock = {
     setItem: function() {},
     removeItem: function() {}
-  };
+  }
 
   it('should report true localStorage is defined and does not throw an exception', function() {
-    window.localStorage = mock;
-    expect(supportsLocalstorage()).to.be.true;
-  });
+    window.localStorage = mock
+    expect(supportsLocalstorage()).to.be.true
+  })
 
   it('should report false if setItem throws an exception', function() {
-    window.localStorage = mock;
-    mock.setItem = function() { throw new Error; };
-    expect(supportsLocalstorage()).to.be.false;
+    window.localStorage = mock
+    mock.setItem = function() { throw new Error }
+    expect(supportsLocalstorage()).to.be.false
   })
-});
+})
